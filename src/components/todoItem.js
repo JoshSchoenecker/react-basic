87f1bfccf79ba8f2b0a915624891010c28facd1a
Josh Schoenecker <schoeneckerj@gmail.com>
2020-06-17 17:09:25 -0600

    TodoItem

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
-export class todoItem extends Component {
-    render() {
-        return (
-            <div>
-                <p>Hello</p>
-            </div>
-        )
-    }
+export class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+          {/* using an expression  */}
+        <p>{this.props.todo.title}</p> 
+      </div>
+    );
+  }
 }
 
-export default todoItem
+//  PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+  }
+export default TodoItem;
